6457742b9832bda1f99dc9e518ab3e83a0f7148c
786abbasi <147985621+786abbasi@users.noreply.github.com>
2024-04-06 03:48:57 +0500

    push mu currency-converter to github

---
+++ index.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1, //Base currency
+    INR: 83.29, //Indian Rupees
+    PKR: 278, //Pakistani Rupees
+    SAR: 3.71, //Saudi Riyal
+    CHF: 0.9, //Swiss France
+    GBP: 0.79, //British Pound
+};
+let userAnswer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter from currency",
+        type: "list",
+        choices: ["USD", "INR", "PKR", "SAR", "CHF", "GBP"],
+    },
+    {
+        name: "to",
+        message: "Enter to currency",
+        type: "list",
+        choices: ["USD", "INR", "PKR", "SAR", "CHF", "GBP"],
+    },
+    {
+        name: "Amount",
+        message: "Enter your Amount",
+        type: "number",
+    },
+]);
+let fromAmount = currency[userAnswer.from];
+let toAmount = currency[userAnswer.to];
+let Amount = userAnswer.Amount;
+let baseAmount = Amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
